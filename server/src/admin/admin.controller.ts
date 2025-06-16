1bdc816982779a02c06ff9e51091257df73cb26d
Jonaskop44 <jonas@codeflexx.com>
2025-06-16 21:11:34 +0200

    changes

--- server/src/admin/admin.controller.ts
+++ server/src/admin/admin.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { JwtAuthGuard } from 'src/guard/jwt-auth.guard';
 import { CreateEmployeeDto } from './dto/admin.dto';
@@ -14,9 +24,31 @@ export class AdminController {
     return this.adminService.getAllEmployees(userId);
   }
 
+  @Get('employee/:id')
+  async getEmployeeById(@Param('id') employeeId: number, @Request() request) {
+    const userId = request.user.id;
+    return this.adminService.getEmployeeById(employeeId, userId);
+  }
+
   @Post('employee')
   async createEmployee(@Body() dto: CreateEmployeeDto, @Request() request) {
     const userId = request.user.id;
     return this.adminService.createEmployee(dto, userId);
+  }
+
+  @Patch('employee/:id')
+  async updateEmployee(
+    @Param('id') employeeId: number,
+    @Body() dto: CreateEmployeeDto,
+    @Request() request,
+  ) {
+    const userId = request.user.id;
+    return this.adminService.updateEmployee(employeeId, dto, userId);
+  }
+
+  @Delete('employee/:id')
+  async deleteEmployee(@Param('id') employeeId: number, @Request() request) {
+    const userId = request.user.id;
+    return this.adminService.deleteEmployee(employeeId, userId);
   }
 }
--- server/src/admin/admin.service.ts
+++ server/src/admin/admin.service.ts
@@ -1,4 +1,8 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateEmployeeDto } from './dto/admin.dto';
 
@@ -12,6 +16,19 @@ export class AdminService {
         userId: userId,
       },
     });
+  }
+
+  async getEmployeeById(employeeId: number, userId: number) {
+    const employee = await this.prisma.employee.findFirst({
+      where: {
+        id: employeeId,
+        userId: userId,
+      },
+    });
+
+    if (!employee) throw new NotFoundException('Employee not found');
+
+    return employee;
   }
 
   async createEmployee(dto: CreateEmployeeDto, userId: number) {
@@ -30,6 +47,53 @@ export class AdminService {
         firstName: dto.firstName,
         lastName: dto.lastName,
         userId: userId,
+      },
+    });
+  }
+
+  async updateEmployee(
+    employeeId: number,
+    dto: CreateEmployeeDto,
+    userId: number,
+  ) {
+    const employee = await this.getEmployeeById(employeeId, userId);
+    if (!employee) throw new NotFoundException('Employee not found');
+
+    if (
+      dto.firstName === employee.firstName &&
+      dto.lastName === employee.lastName
+    )
+      throw new ConflictException('No changes detected');
+
+    const existingEmployee = await this.prisma.employee.findFirst({
+      where: {
+        firstName: dto.firstName,
+        lastName: dto.lastName,
+      },
+    });
+
+    if (existingEmployee && existingEmployee.id !== employee.id)
+      throw new ConflictException('Employee with this name already exists');
+
+    return this.prisma.employee.update({
+      where: {
+        id: employee.id,
+      },
+      data: {
+        firstName: dto.firstName,
+        lastName: dto.lastName,
+      },
+    });
+  }
+
+  async deleteEmployee(employeeId: number, userId: number) {
+    const employee = await this.getEmployeeById(employeeId, userId);
+
+    if (!employee) throw new NotFoundException('Employee not found');
+
+    return this.prisma.employee.delete({
+      where: {
+        id: employee.id,
       },
     });
   }
